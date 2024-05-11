c628e95f69aed3411c09a01615c3c898c6136234
xmohammad1 <117955130+xmohammad1@users.noreply.github.com>
2024-05-11 04:54:02 +0330

    Update run.sh

--- run.sh
+++ run.sh
@@ -109,5 +109,6 @@ if [[ "$choices" -eq 1 || "$choices" -eq 2 ]]; then
 
   if [ "$choices" -eq 2 ]; then
   echo "Local IPv6 Kharej: 2001:470:1f10:e1f::2"
+  echo "Local Ipv6 Iran: 2001:470:1f10:e1f::2"
   fi
 fi
