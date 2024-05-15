ecbc46f8070d24c2631e1c2e27629d3ce8ce3b96
xmohammad1 <117955130+xmohammad1@users.noreply.github.com>
2024-05-15 23:19:58 +0330

    Update run.sh

--- run.sh
+++ run.sh
@@ -85,6 +85,7 @@ sysctl -p
   sleep 0.5
   echo "$rctext" > /etc/rc.local
 elif [ "$choices" -eq 3 ]; then
+  echo > /etc/rc.local
   sudo mv /root/rc.local.old /etc/rc.local
   ip link show | awk '/6to4tun/ {split($2,a,"@"); print a[1]}' | xargs -I {} ip link set {} down
   ip link show | awk '/6to4tun/ {split($2,a,"@"); print a[1]}' | xargs -I {} ip tunnel del {}
